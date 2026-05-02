b3e77b46f74d9e877c4c0b00246a12b518091c80
Michael Timothy <timmichael738@gmail.com>
2026-05-02 22:53:43 +0100

    Vite config file base value updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Era-Connect/cdp_repo.git'
+  base: '/cdp_repo/'
 })
